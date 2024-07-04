149fee13ed1bb4f151fb7cb037e776ce6bc47702
Ivo Sorokins <91875727+IvoSorokins@users.noreply.github.com>
2024-07-04 17:05:48 +0300

    Added Tags to Runners

--- src/test/java/runner/AndroidTestRunner.java
+++ src/test/java/runner/AndroidTestRunner.java
@@ -6,6 +6,7 @@ import io.cucumber.testng.CucumberOptions;
     @CucumberOptions(
             features = {"src/test/resources/features/TestCases.feature"}, // Path to your feature files
             glue = {"definitions.android"},   // Package where your step definitions are located
+            tags = "@PartOne",
             plugin = {"pretty", "html:target/cucumber-reports"}
     )
 
--- src/test/java/runner/IOSTestRunner.java
+++ src/test/java/runner/IOSTestRunner.java
@@ -3,10 +3,13 @@ package runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(
-        features = {"src/test/resources/features/TestCases.feature"}, // Path to your feature files
-        glue = {"definitions.iOS"},   // Package where your step definitions are located
-        plugin = {"pretty", "html:target/cucumber-reports"}
-)
-public class IOSTestRunner  extends AbstractTestNGCucumberTests {
-}
+    @CucumberOptions(
+            features = {"src/test/resources/features/TestCases.feature"}, // Path to your feature files
+            glue = {"definitions.iOS"},   // Package where your step definitions are located
+            tags = "@PartOne",
+            plugin = {"pretty", "html:target/cucumber-reports"}
+    )
+
+    public class IOSTestRunner  extends AbstractTestNGCucumberTests {
+
+    }
