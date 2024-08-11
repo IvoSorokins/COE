ad852b7a65f79fd9872d70d83cf07cc81c5df4ff
Ivo Sorokins <91875727+IvoSorokins@users.noreply.github.com>
2024-08-11 16:10:51 +0300

    Created Step methods

--- src/test/java/definitions/iOS/IOSPartTwo.java
+++ src/test/java/definitions/iOS/IOSPartTwo.java
@@ -48,4 +48,31 @@ public class IOSPartTwo {
             driver.quit();
         }
     }
+
+    // Part Two Specific
+
+
+    @When("I tap on {string} button to open Part 2 screen")
+    public void i_tap_on_button_to_open_part_screen(String string) {
+        homePage.clickPartButton(2);
+        AssertionUtil.assertEquals(taskOnePage.getHeaderTitle(), string, driver);
     }
+
+    @And("I save list items while scrolling through the list")
+    public void i_save_list_items_while_scrolling_through_the_list(){}
+
+    @Then("I validate the saved items are in an alphabetical order")
+    public void i_validate_the_saved_items_are_in_alphabetical_order() {}
+
+
+    @Then("I validate the {string} has a * symbol added")
+    public void i_validate_the_has_a_symbol_added(String itemName) {}
+
+
+    @Then("I validate the {string} categories are empty")
+    public void i_validate_the_categories_are_empty(String categories) {}
+
+    @Then("I validate the {string} categories have 3 or more items")
+    public void i_validate_the_categories_have_3_or_more_items(String categories){}
+
+}
