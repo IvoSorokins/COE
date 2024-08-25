cebb8be56007039a4e7dc51c5bcd1962e4917d61
Ivo Sorokins <91875727+IvoSorokins@users.noreply.github.com>
2024-08-25 20:22:50 +0300

    Debugged Scroll

--- src/main/java/utils/Helpers.java
+++ src/main/java/utils/Helpers.java
@@ -1,10 +1,6 @@
 package utils;
 
 import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.PerformsTouchActions;
-import io.appium.java_client.TouchAction;
-import io.appium.java_client.touch.WaitOptions;
-import io.appium.java_client.touch.offset.PointOption;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.interactions.PointerInput;
 import org.openqa.selenium.interactions.Sequence;
@@ -16,17 +12,7 @@ import java.util.Collections;
 public class Helpers {
 
     // Method to perform scrolling, takes count of scrolls as argument
-    public static void scroll(String platform,String direction, int scrollCount, AppiumDriver driver) {
-        for (int i = 0; i < scrollCount; i++) {
-            if (platform.equals("iOS")) {
-                scrollIOS(driver,direction,scrollCount);
-            } else {
-                scrollAndroid(driver);
-            }
-        }
-    }
-
-    private static void scrollIOS(AppiumDriver driver, String direction, int times) {
+    public static void scroll(String direction, int scrollCount, AppiumDriver driver) {
         // Define the dimensions
         Dimension size = driver.manage().window().getSize();
         int startX = size.width / 2;
@@ -58,7 +44,7 @@ public class Helpers {
         }
 
         // Perform the scroll the specified number of times
-        for (int i = 0; i < times; i++) {
+        for (int i = 0; i < scrollCount; i++) {
             // Create a PointerInput instance representing the finger for touch actions
             PointerInput finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
 
@@ -72,19 +58,5 @@ public class Helpers {
             // Perform the scroll action
             ((RemoteWebDriver) driver).perform(Collections.singletonList(swipe));
         }
-    }
-
-    private static void scrollAndroid(AppiumDriver driver) {
-        Dimension size = driver.manage().window().getSize();
-        int startY = (int) (size.height * 0.8);
-        int endY = (int) (size.height * 0.2);
-        int startX = size.width / 2;
-
-        new TouchAction<>((PerformsTouchActions) driver)
-                .press(PointOption.point(startX, startY))
-                .waitAction(WaitOptions.waitOptions(Duration.ofSeconds(1)))
-                .moveTo(PointOption.point(startX, endY))
-                .release()
-                .perform();
     }
 }
