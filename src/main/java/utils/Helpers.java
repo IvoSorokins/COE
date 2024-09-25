98e2348e2ff5b80e4eb42ed2708f25a53be5d0af
Ivo Sorokins <91875727+IvoSorokins@users.noreply.github.com>
2024-09-25 16:12:48 +0300

    Code Cleanup

--- src/main/java/utils/Helpers.java
+++ src/main/java/utils/Helpers.java
@@ -2,13 +2,14 @@ package utils;
 
 import io.appium.java_client.AppiumDriver;
 import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.PointerInput;
 import org.openqa.selenium.interactions.Sequence;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.remote.RemoteWebElement;
 
 import java.time.Duration;
 import java.util.Collections;
+
 
 public class Helpers {
 
@@ -87,7 +88,7 @@ public class Helpers {
     }
 
     // Method to check if element is visible
-    public static boolean isElementVisible(RemoteWebElement element) {
+    public static boolean isElementVisible(WebElement element) {
         try {
             return element.isDisplayed();
         } catch (Exception e) {
